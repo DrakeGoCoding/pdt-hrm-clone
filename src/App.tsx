3416cc37c2755c197996e5c920203e4b6a1e4840
Phí Hoàng Long <57493556+DrakeGoCoding@users.noreply.github.com>
2022-11-25 00:39:27 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <form>
+        <input
+          id="dinosaur_name"
+          name="dinosaur_name"
+          className="dinosaur-input"
+          aria-describedby="dinosaur_name_info"
+        />
+      </form>
+    </div>
+  );
+}
+
+export default App;
